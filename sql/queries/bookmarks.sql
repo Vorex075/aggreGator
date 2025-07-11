a40356c9584025da200d5e65d0e336ff05931128
Roberto <roberto@losgimenez.com>
2025-07-11 20:34:16 +0200

    Updated paginated bookmark to return information about the post itself

--- sql/queries/bookmarks.sql
+++ sql/queries/bookmarks.sql
@@ -12,8 +12,9 @@ FROM bookmarks
 WHERE user_id = $1 AND post_id = $2;
 
 -- name: GetUserBookmarks :many
-SELECT *
+SELECT bookmarks.*, posts.title, posts.url, posts.description, posts.published_at 
 FROM bookmarks
+INNER JOIN posts ON bookmarks.post_id = posts.id
 WHERE user_id = $1 AND (created_at < $2 OR (created_at < $2 AND post_id < $3))
 ORDER BY created_at DESC, post_id DESC
 LIMIT $4;
