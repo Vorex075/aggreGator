213c850c9527ed8d5bd25bc22f20ee223a886380
Roberto <roberto@losgimenez.com>
2025-07-11 20:10:38 +0200

    Small adition to bookmarks

--- sql/schema/006_bookmarks.sql
+++ sql/schema/006_bookmarks.sql
@@ -2,13 +2,16 @@
 CREATE TABLE bookmarks(
   user_id uuid,
   post_id INTEGER,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
   PRIMARY KEY (user_id, post_id),
   CONSTRAINT fk_user
   FOREIGN KEY (user_id)
-  REFERENCES users(id),
+  REFERENCES users(id)
+  ON DELETE CASCADE,
   CONSTRAINT fk_post
   FOREIGN KEY (post_id)
   REFERENCES posts(id)
+  ON DELETE CASCADE
 );
 
 -- +goose Down
